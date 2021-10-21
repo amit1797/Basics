c23bec0d3febe01249f9904abf9c50beac737a82
root <root@localhost>
2021-10-21 14:46:59 +0530

    auto publish 1634807819705

--- src/week2/bmi_calculator.py
+++
@@ -1,6 +0,0 @@
-height = float(input("Enter your height in meters: "))
-weight = float(input("Enter your weight in kg: "))
-
-bmi = int(weight/height**2)
-
-print('Your BMI is ' + str(bmi))
--- src/week3/odd_even.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Which number do you want to check? "))
-if num%2==0:
-  print(f"{num} is a Even Number")
-else:
-  print(f"{num} is a Odd Number")
